dc7bf04f4946b73b02f53903120eda3422e0b77c
Martin <martin_de_v@hotmail.com>
2024-02-20 15:49:58 +0100

    refactor of Quiz.jsx

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,6 +5,7 @@ import { Form, Button, Container } from "react-bootstrap";
 import "./quiz.css";
 import QuizAnimation from "../components/QuizAnimation";
 import { fetchData, putData } from "utils/api";
+import QuizInitialization from "./QuizInitialization";
 
 const Quiz = () => {
 	let user_id = localStorage.getItem("languagelearning_id");
@@ -214,61 +215,20 @@ const Quiz = () => {
 
 					</div>
 				)}{" "}
-			{!gameStarted && (
-				<div
-					style={{
-						display: "flex",
-						flexDirection: "column",
-						justifyContent: "flex-start",
-						width: "15%",
-					}}
-				>
-					<label htmlFor="lang1">Choose language 1:</label>
-					<select
-						id="lang1"
-						value={language1}
-						onChange={handleLanguage1Change}
-						style={{ width: "50%" }}
-					>
-						<option value="NL">NL</option>
-						<option value="EN">EN</option>
-						<option value="EN-GB">EN-GB</option>
-						<option value="ES">ES</option>
-					</select>
-
-					<label htmlFor="lang2">Choose language 2:</label>
-					<select
-						id="lang2"
-						value={language2}
-						onChange={handleLanguage2Change}
-						style={{ width: "50%" }}
-					>
-						<option value="NL">NL</option>
-						<option value="EN">EN</option>
-						<option value="EN-GB">EN-GB</option>
-						<option value="ES">ES</option>
-					</select>
-
-					<label htmlFor="quiztype">Choose a quiz type:</label>
-					<select
-						id="quiztype"
-						value={quizType}
-						onChange={handleQuizTypeChange}
-						style={{ width: "100%" }}
-					>
-						<option>Select quiz type</option>
-						<option value="all">All</option>
-						<option value="wrong">Top 10 wrong answers</option>
-						<option value="least_attempts">Least practiced words</option>
-						<option value="flagged">Flagged</option>
-					</select>
-
-					<button onClick={startGame}>Start</button>
-				</div>
-			)}
 			{currentQuestionIndex > 0 && (
 				<Button onClick={restartQuiz}>Restart</Button>
 			)}
+			{!gameStarted &&
+				<QuizInitialization
+					language1={language1}
+					language2={language2}
+					handleLanguage1Change={handleLanguage1Change}
+					handleLanguage2Change={handleLanguage2Change}
+					quizType={quizType}
+					handleQuizTypeChange={handleQuizTypeChange}
+					startGame={startGame}
+				/>}
+
 			<QuizAnimation {...{ currentQuestionIndex, questions }}></QuizAnimation>
 		</Container>
 	);
