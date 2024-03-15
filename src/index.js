27304c997eeb478272bcec923af7a0d88c8f927e
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2024-03-15 14:02:23 +0100

    Add auth provider to app

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "utils/AuthContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<AuthProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</AuthProvider>
 	</React.StrictMode>
 );
 
